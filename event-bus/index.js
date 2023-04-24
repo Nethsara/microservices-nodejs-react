dad1fc59cfafae1f5bc6f290fea4bb409d524f48
Nethsara Siyum <nethsarasiyum2@gmail.com>
2023-04-25 00:36:47 +0530

    Create Query Service

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,9 +14,9 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  // axios.post("http://localhost:4002/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
 });
 
